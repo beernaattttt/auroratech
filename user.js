b7012c1b6c6ce4ff320bee7f0168a9defe143457
auroratech-roblox <auroratechnology.roblox@gmail.com>
2025-01-06 19:10:09 +0100

    Delete user.js

--- user.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    if (req.session && req.session.user) {
-      return res.status(200).json(req.session.user);
-    }
-    return res.status(401).json({ message: 'Not authenticated' });
-  }
-}
